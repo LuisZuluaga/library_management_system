107bee8df629a01559c285cd5b9874b07022d2be
Luis Zuluaga <luiszv_211@hotmail.com>
2025-05-30 18:52:26 -0500

    Add dashboards for librarians and members with corresponding views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,12 @@ class DashboardController < ApplicationController
       @borrowed_books = Borrowing.where.not(returned_at: nil)
       @due_today = Borrowing.where(due_at: Date.today)
       @overdue_members = User.joins(:borrowings).where('borrowings.due_at < ? AND borrowings.returned_at IS NULL', Date.today).distinct
+
+      render 'librarian_dashboard'
     elsif current_user.member?
       @my_books = current_user.borrowings.includes(:book)
+
+      render 'member_dashboard'
     end
   end
 end
