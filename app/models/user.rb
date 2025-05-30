874da7af3aacf3dd57f0bc17853aee24c3c6d00e
Luis Zuluaga <luiszv_211@hotmail.com>
2025-05-30 17:15:46 -0500

    Add User model with Devise integration and initial tests

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  enum role: { librarian: 0, member: 1 }
+
+  # Validations
+  validates :name, presence: true
+end
