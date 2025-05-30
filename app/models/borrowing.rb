09f47405cdc05f9b414b35bfe6194b629d51476e
Luis Zuluaga <luiszv_211@hotmail.com>
2025-05-30 17:28:22 -0500

    Add Borrowing model with associations, validations, and due date calculation

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -3,4 +3,10 @@ class Borrowing < ApplicationRecord
   belongs_to :book
 
   validates :borrowed_at, :due_at, presence: true
+
+  before_validation :set_due_at, on: :create
+
+  def set_due_at
+    self.due_at ||= borrowed_at + 2.weeks if borrowed_at
+  end
 end
